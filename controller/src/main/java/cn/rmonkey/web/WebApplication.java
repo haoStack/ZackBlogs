ba7393d888ca3eacf984e19b855af5a5ed1fadc6
Zack <haoStack@163.com>
2022-03-27 23:40:54 +0800

    模块间依赖配置完成，成功启动第一个模块化项目

--- controller/src/main/java/cn/rmonkey/web/WebApplication.java
+++ controller/src/main/java/cn/rmonkey/web/WebApplication.java
@@ -2,7 +2,9 @@ package cn.rmonkey.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan("cn.rmonkey")
 @SpringBootApplication
 public class WebApplication {
     public static void main(String[] args) {
--- controller/src/main/java/cn/rmonkey/web/controller/Controller.java
+++ controller/src/main/java/cn/rmonkey/web/controller/Controller.java
@@ -2,11 +2,12 @@ package cn.rmonkey.web.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/xhBlogs")
+@RestController
 public class Controller {
     @ResponseBody
-    @RequestMapping("/hello")
+    @RequestMapping("/")
     public String hello() {
         return "hello";
     }
